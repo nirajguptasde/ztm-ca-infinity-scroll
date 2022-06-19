7bd550b3f1693e43a41e7a7a6c294d151c681edb
Niraj Gupta <nirajguptasde@gmail.com>
2022-06-19 21:04:36 +0530

    got infinite load working

--- script.js
+++ script.js
@@ -3,13 +3,27 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = "rZPCvRkCOdzJEK3N8FEeUcfLSMjVgwF_OTXHiQvxxfA"
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// check if all images were loaded
+
+function imageLoaded(){
+  
+  imagesLoaded++;
+  console.log('image loaded');
+  if(imagesLoaded === totalImages) {
+    ready = true;
+    console.log('ready = ', ready);
+  }
+}
 
 // helper function
 
@@ -22,9 +36,11 @@ function setAttributes(element, attributes) {
 
 //create elements for links and photos, add to dom
 function displayPhotos(){
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log('total images', totalImages);
   // run a callback function for each object in photos array
   photosArray.forEach((photo) => {
-    console.log(photo);
     // create <a>element</a> that links back to unsplash
     const item = document.createElement('a');
     setAttributes(item, {
@@ -38,6 +54,8 @@ function displayPhotos(){
       alt: photo.alt_description,
       title: photo.alt_description
     });
+    // Add an event listner to check if all the photos have finished loading
+    img.addEventListener('load', imageLoaded)
     // put the image inside the anchor element, then put them inside the imageContainer element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -50,7 +68,6 @@ async function getPhotosFromUnsplashApi(){
     const response = await fetch(apiUrl);
     photosArray = await response.json();
     displayPhotos()
-    console.log('yay')
   } catch(error) {
     console.log(error);
   } 
@@ -59,8 +76,11 @@ async function getPhotosFromUnsplashApi(){
 // check to see if scrolling near botton of page, load more photos
 
 window.addEventListener('scroll', () => {
-  if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
-    console.log(window.innerHeight);
+  if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotosFromUnsplashApi();
+    imageLoaded()
+
   }
 });
 
